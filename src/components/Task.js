60ba900efce287d1013f34205617e0d1d0543df5
Mehul Desai <desaimehul94@gmail.com>
2022-04-30 20:42:46 -0400

    working on task

---
+++ src/components/Task.js
@@ -0,0 +1,17 @@
+
+
+const Task = ({}) => {
+    return(
+        <div>
+            <div className="task">
+                <div>
+                    <p className="taskName">
+                        <span className="textBold">Task Name:</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Task;
