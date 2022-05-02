8708f20df24ef607b21efd4b065d1daf7f42e5f5
Mehul Desai <desaimehul94@gmail.com>
2022-05-02 19:50:05 -0400

    task component done

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,23 @@
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
+import "../index.css"
 
-
-const Task = ({}) => {
-    return(
+const Task = ({ task, onDelete, onEdit }) => {
+    return (
         <div>
             <div className="task">
                 <div>
                     <p className="taskName">
-                        <span className="textBold">Task Name:</span>
+                        <span className="textBold">Task Name:</span>{task.task}
                     </p>
+                    <p className="taskDate">
+                        <span className="textBold">
+                            Date of Completion:
+                        </span>
+                    </p>
+                </div>
+                <div>
+                    <p><FaTimes onClick={() => onDelete(task.id)} className="delIcon" /></p>
+                    <p><FaPencilAlt onClick={() => onEdit(task.id)} className="editIcon" /></p>
                 </div>
             </div>
         </div>
