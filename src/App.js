8ea9748757360286a96603f2571912f88ba44a2a
Mehul Desai <desaimehul94@gmail.com>
2022-05-06 20:58:19 -0400

    app.js added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import './App.css';
 import AddTask from './components/AddTask';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello World!!</h1>
-      <AddTask />
-    </div>
 
+import { useState, useEffect } from 'react'
+
+import { v4 as uuidv4 } from 'uuid';
+import Swal from 'sweetalert2';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+
+
+
+function App() {
+
+  const [tasks, setTasks] = useState([]);
+  const [showAddTask, setShowAddTask] = useState(false);
+
+
+
+  useEffect(() => {
+    alert('Welcome to our app')
+  }, []);
+
+ const addTask = (task) => {
+
+ }
+
+  return (
+    <>
+      <div className='container'>
+        <Header showForm={() => setShowAddTask(!showAddTask)} changeTextColor={showAddTask} />
+
+        {showAddTask && <AddTask onSave={addTask} />}
+
+        {
+          tasks.length > 0 ?
+            (<Tasks tasks={tasks} />) :
+            ('No Task Found !')
+        }
+
+      </div>
+    </>
   );
 }
 
